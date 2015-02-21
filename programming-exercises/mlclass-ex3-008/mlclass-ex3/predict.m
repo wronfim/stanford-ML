cc9012454dc3cec67d85f64e47d97d47bae6ca54
wellinthon <wronfim@gmail.com>
2015-02-21 15:24:01 -0500

    Neural network prediction function

--- programming-exercises/mlclass-ex3-008/mlclass-ex3/predict.m
+++ programming-exercises/mlclass-ex3-008/mlclass-ex3/predict.m
@@ -21,13 +21,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1), X];
 
+hidden = [ones(m,1), sigmoid(X * Theta1')];
 
+output = sigmoid(hidden * Theta2');
 
-
-
-
-
+[value,p] = max(output,[],2);
 
 % =========================================================================
 
