f1ccc2c10843ba626338f113368ef8d40c59f8d4
wellinthon <wronfim@gmail.com>
2015-03-09 16:05:39 -0400

    Polynomial Feature Mapping

--- programming-exercises/mlclass-ex5-008/mlclass-ex5/polyFeatures.m
+++ programming-exercises/mlclass-ex5-008/mlclass-ex5/polyFeatures.m
@@ -6,7 +6,7 @@ function [X_poly] = polyFeatures(X, p)
 %
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p)
+X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
