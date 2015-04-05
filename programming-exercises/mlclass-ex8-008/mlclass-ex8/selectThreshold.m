4b5fdb87719a57acfc80b68deb5a96c71785614e
Wellinthon Ronfim <wronfim@gmail.com>
2015-04-05 14:07:25 -0400

    Select Threshold

--- programming-exercises/mlclass-ex8-008/mlclass-ex8/selectThreshold.m
+++ programming-exercises/mlclass-ex8-008/mlclass-ex8/selectThreshold.m
@@ -23,17 +23,15 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    h = pval < epsilon;
 
+    tp = sum((yval == 1) & (h == 1));
+    fp = sum((yval == 0) & (h == 1));
+    fn = sum((yval == 1) & (h == 0));
 
-
-
-
-
-
-
-
-
-
+    prec = tp/(tp+fp);
+    rec = tp/(tp+fn);
+    F1 = (2*prec*rec)/(prec+rec);
 
     % =============================================================
 
