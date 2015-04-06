b5d9671faf706f23c45002d83793ad04b83c6b55
Wellinthon Ronfim <wronfim@gmail.com>
2015-04-05 20:25:46 -0400

    Regularized Gradient

--- programming-exercises/mlclass-ex8-008/mlclass-ex8/cofiCostFunc.m
+++ programming-exercises/mlclass-ex8-008/mlclass-ex8/cofiCostFunc.m
@@ -42,8 +42,8 @@ Theta_grad = zeros(size(Theta));
 
 J = (1/2) * sum(sum((X*Theta' - Y).^2 .* R)) + (lambda/2)*( sum(sum(Theta.^2)) + sum(sum(X.^2)));
 
-X_grad = ((X*Theta' - Y) .* R) * Theta;
-Theta_grad = ((X*Theta' - Y) .* R)' * X;
+X_grad = ((X*Theta' - Y) .* R) * Theta + lambda*X;
+Theta_grad = ((X*Theta' - Y) .* R)' * X + lambda*Theta;
 
 % =============================================================
 
